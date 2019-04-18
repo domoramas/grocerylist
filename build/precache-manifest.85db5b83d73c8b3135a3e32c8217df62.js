63114c8496a05a0e83af3d9c3c5bab9c707e05e6
domoramas <boombappro@gmail.com>
2019-04-18 11:26:11 -0700

    update

---
+++ build/precache-manifest.85db5b83d73c8b3135a3e32c8217df62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d7a21b4f33b9e64ea0",
+    "url": "/grocerylist/static/js/runtime~main.a382c8a9.js"
+  },
+  {
+    "revision": "b9b75e9a29e656e12af4",
+    "url": "/grocerylist/static/js/main.3854ee45.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/grocerylist/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "b9b75e9a29e656e12af4",
+    "url": "/grocerylist/static/css/main.929165a7.chunk.css"
+  },
+  {
+    "revision": "ea3092a8690f1bd10fbbc0381c89669c",
+    "url": "/grocerylist/index.html"
+  }
+];
