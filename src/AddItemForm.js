e4710af4113d6eb29c4173adf23158a710dccf02
domoramas <boombappro@gmail.com>
2019-04-22 14:10:46 -0700

    cleaned up additemform

--- src/AddItemForm.js
+++ src/AddItemForm.js
@@ -1,38 +1,4 @@
 import React, { Component } from "react";
-// import PropTypes from "prop-types"
-
-// const AddItemForm = ({ addItem }) => {
- 
-//  let itemInput = React.createRef();
-//  let handleSubmit = (e) => {
-//     e.preventDefault();
-//     addItem(itemInput.current.value);
-//     e.currentTarget.reset();
-//   }
- 
-//   console.log(itemInput.current.value)
-//   return (
-  
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         type="text"
-//         ref= { itemInput } 
-//         placeholder="Enter an item"
-//       />
-
-//         <input
-          
-//           type="submit"
-//           value = "Add Item"
-//         />
-//     </form>
-
-//   );
-// }
-// AddItemForm.propTypes = {
-//   addItem: PropTypes.func,
-// }
-// export default AddItemForm;
 
 class AddItemForm extends Component {
 
