5a05f73107443288bc1656c65685d966573b354c
domoramas <boombappro@gmail.com>
2019-04-22 14:08:39 -0700

    conditionally disabled submit button when no text in field

--- src/AddItemForm.js
+++ src/AddItemForm.js
@@ -1,33 +1,70 @@
-import React from "react";
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+// import PropTypes from "prop-types"
 
-const AddItemForm = ({ addItem }) => {
+// const AddItemForm = ({ addItem }) => {
  
- let itemInput = React.createRef();
- let handleSubmit = (e) => {
-    e.preventDefault();
-    addItem(itemInput.current.value);
-    e.currentTarget.reset();
-  }
-  const hasInput = addItem.name.length > 0;
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        ref= { itemInput } 
-        placeholder="Enter an item"
-      />
+//  let itemInput = React.createRef();
+//  let handleSubmit = (e) => {
+//     e.preventDefault();
+//     addItem(itemInput.current.value);
+//     e.currentTarget.reset();
+//   }
+ 
+//   console.log(itemInput.current.value)
+//   return (
+  
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         ref= { itemInput } 
+//         placeholder="Enter an item"
+//       />
 
+//         <input
+          
+//           type="submit"
+//           value = "Add Item"
+//         />
+//     </form>
+
+//   );
+// }
+// AddItemForm.propTypes = {
+//   addItem: PropTypes.func,
+// }
+// export default AddItemForm;
+
+class AddItemForm extends Component {
+
+  state = {
+    value: ""
+  };
+  handleValueChange = (e) => {
+    this.setState({value : e.target.value }); 
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addItem(this.state.value);
+    this.setState({value: ""});
+  }
+
+  render(){
+    console.log(this.state.value)
+    return (
+      <form onSubmit={this.handleSubmit}>
         <input
-          disabled = {!hasInput}
+          type= "text"
+          value= {this.state.value}
+          onChange={this.handleValueChange}
+          placeholder= "Enter an item"
+        />
+        <input
           type="submit"
           value = "Add Item"
+          disabled = {!this.state.value}
         />
-    </form>
-
-  );
-}
-AddItemForm.propTypes = {
-  addItem: PropTypes.func,
+      </form>
+    )
+  }
 }
 export default AddItemForm;
