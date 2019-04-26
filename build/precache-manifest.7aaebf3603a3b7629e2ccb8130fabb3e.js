7d28d6e170fcad72dcc35969703cef38494bc6a1
domoramas <boombappro@gmail.com>
2019-04-26 14:55:13 -0700

    reverted to predeployment and ran run build

---
+++ build/precache-manifest.7aaebf3603a3b7629e2ccb8130fabb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d7a21b4f33b9e64ea0",
+    "url": "/grocerylist/static/js/runtime~main.a382c8a9.js"
+  },
+  {
+    "revision": "deb28401ee3a344d9c05",
+    "url": "/grocerylist/static/js/main.029722c3.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/grocerylist/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "deb28401ee3a344d9c05",
+    "url": "/grocerylist/static/css/main.929165a7.chunk.css"
+  },
+  {
+    "revision": "3d4818f53eb71b22a6642631a65603e5",
+    "url": "/grocerylist/index.html"
+  }
+];
